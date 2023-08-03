671321d22aa0e00871d972de51f50cafa702f685
Sascha Koeppe <saschakoeppe@gmail.com>
2023-08-03 15:08:15 +0900

    add destroy user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to root_path, status: :see_other
+
   private
 
   def user_params
