ef55fd928e5e13bdc43c09b31290fb6992eb5de4
Sascha Koeppe <saschakoeppe@gmail.com>
2023-08-03 10:50:08 +0900

    generate users and posts controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :user
+  resources :post, only: [:new, :create, :index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
